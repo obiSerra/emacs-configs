0e43664e3299367ae3cde1b7b39459e76a74f508
Roberto <>
2018-09-28 00:42:14 +0200

    react snippet in progress

--- customizations/functions.el
+++ customizations/functions.el
@@ -1,3 +1,5 @@
+;; ((lambda (s) (let ((case-fold-search nil)) (while (string-match "[A-Z]" s 1) (setq s (replace-match (concat "-" (downcase (match-string 0 s))) t nil s))) (downcase s))) yas-text)
+
 (defun camelCase->snake-case (s &optional sep start)
   "Convert CamelCase string S to lower case with word separator SEP.
    Default for SEP is a hyphen \"-\"."
