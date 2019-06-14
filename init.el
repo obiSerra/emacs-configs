feae5035fabb3fd1f6ed6f63a5315f7096720d9a
Roberto <>
2019-06-14 22:32:41 +0200

    Missing plugins added

--- init.el
+++ init.el
@@ -46,8 +46,6 @@
     ;; key bindings and code colorization for Clojure
     ;; https://github.com/clojure-emacs/clojure-mode
     clojure-mode
-
-
 ;    js2-mode
     ;; extra syntax highlighting for clojure
     clojure-mode-extra-font-locking
@@ -85,7 +83,11 @@
     ;; lsp-mode
     ;; lsp-ui
     ;; company-lsp
-    dash))
+    dash
+    hide-comnt
+    flycheck
+    spacemacs-theme
+    prettier-js))
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
@@ -168,7 +170,7 @@
 (load "setup-org.el")
 
 ;; Private data and password
-(load "private.el")
+(load "private.el" t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -191,7 +193,7 @@
   %a"))) t)
  '(package-selected-packages
    (quote
-    (dash ruby-mode hide-comnt flycheck flow-minor-mode markdown-preview-mode websocket spiral yasnippet vue-mode vue-html-mode markdown-mode+ prettier-js scheme-complete scribble-mode geiser quack curl-for-url less sass-mode org-brain geeknote ipython column-marker rjsx-mode json-mode handlebars-mode dockerfile-mode js-auto-beautify js2-mode feature-mode less-css-mode kibit-helper gherkin-mode fiplr yaml-mode arduino-mode flymake-jslint ## racket-mode ssh apache-mode php-mode atom-dark-theme spacemacs-theme darkokai-theme groovy-mode ox-twbs plantuml-mode request web-mode org-gcal php+-mode zenburn tagedit smex rainbow-delimiters projectile paredit newlisp-mode monokai-theme material-theme markdown-mode magit jenkins ido-ubiquitous highlight-symbol exec-path-from-shell cyberpunk-theme clojure-mode-extra-font-locking cider ac-emmet))))
+    (dash ruby-mode flycheck flow-minor-mode markdown-preview-mode websocket spiral yasnippet vue-mode vue-html-mode markdown-mode+ prettier-js scheme-complete scribble-mode geiser quack curl-for-url less sass-mode org-brain geeknote ipython column-marker rjsx-mode json-mode handlebars-mode dockerfile-mode js-auto-beautify js2-mode feature-mode less-css-mode kibit-helper gherkin-mode fiplr yaml-mode arduino-mode flymake-jslint ## racket-mode ssh apache-mode php-mode atom-dark-theme spacemacs-theme darkokai-theme groovy-mode ox-twbs plantuml-mode request web-mode org-gcal php+-mode zenburn tagedit smex rainbow-delimiters projectile paredit newlisp-mode monokai-theme material-theme markdown-mode magit jenkins ido-ubiquitous highlight-symbol exec-path-from-shell cyberpunk-theme clojure-mode-extra-font-locking cider ac-emmet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
