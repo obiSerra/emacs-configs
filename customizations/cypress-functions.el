6d994a2046aaf677ee5a46d81d7564415e110f68
Roberto Serra <obi.serra@gmail.com>
2019-07-30 10:52:29 +0200

    navigate back to previous cursor point

--- customizations/cypress-functions.el
+++ customizations/cypress-functions.el
@@ -1,26 +1,23 @@
-;;; WHEN get back to the starting char, count the chars added/removed
-
-
 (defun cy-skip-all ()
   (interactive)
-
-  (let (cursor-original-position (point))
+  (let ((cursor-original-position (point)))
+    (print cursor-original-position)
     (goto-char (point-min))
     (while (re-search-forward "^\\([[:blank:]]*it\\)(" nil t)
       (replace-match "\\1.skip("))
     (goto-char cursor-original-position)))
 
 (defun cy-unskip-all ()
-  (interactive)
-  (let (cursor-original-position (point))
-    (goto-char (point-min))
-    (while (re-search-forward "^\\([[:blank:]]*it\\)\.skip" nil t)
-      (replace-match "\\1"))
-    (goto-char cursor-original-position)))
+    (interactive)
+    (let ((cursor-original-position (point)))
+      (goto-char (point-min))
+      (while (re-search-forward "^\\([[:blank:]]*it\\)\.skip" nil t)
+        (replace-match "\\1"))
+      (goto-char cursor-original-position)))
 
 (defun cy-skip-current ()
   (interactive)
-  (let (cursor-original-position (point))
+  (let ((cursor-original-position (point)))
     (re-search-backward "^\\([[:blank:]]*it\\)" nil t)
     (replace-match "\\1.skip")
     (goto-char cursor-original-position)))
@@ -28,7 +25,7 @@
 
 (defun cy-unskip-current ()
   (interactive)
-  (let (cursor-original-position (point))
+  (let ((cursor-original-position (point)))
        (re-search-backward "^\\([[:blank:]]*it\\)\.skip" nil t)
        (replace-match "\\1")
-       (goto-char (+ 5 cursor-original-position))))
+       (goto-char cursor-original-position)))
