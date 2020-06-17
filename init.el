317da7faa44213f1e9546979b8a6f6c630f4e531
root <root@kali>
2020-06-17 10:33:01 +0000

    compatibility with different version of emacs and tls

--- init.el
+++ init.el
@@ -5,6 +5,16 @@
 ;; Define package repositories
 (require 'package)
 (package-initialize)
+
+;; Some combination of GNU TLS and Emacs fail to retrieve archive
+;; contents over https.
+;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+
+(if (and (version< emacs-version "26.3") (>= libgnutls-version 30604))
+    (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
+
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
