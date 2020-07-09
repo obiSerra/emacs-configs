fae93db4dca976518a961a5af42aff80b6322ee1
Roberto Serra <obi.serra@gmail.com>
2020-07-09 09:29:54 +0200

    emphasis

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -61,7 +61,9 @@
 ;; Display emphasis 
 (setq org-hide-emphasis-markers t)
 ;; Better list bullets
-(setq org-hide-emphasis-markers t)
+(font-lock-add-keywords 'org-mode
+                        '(("^ +\\([-*]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 ;; Better headers
